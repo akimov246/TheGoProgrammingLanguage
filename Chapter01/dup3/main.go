4ddb99ac90f83e89c0162e032c88b7484ea47ff7
Leonid Akimov <akimov246>
2026-08-11 15:29:28 +0700

    done: 1.3. Поиск повторяющихся строк

---
+++ Chapter01/dup3/main.go
@@ -0,0 +1,29 @@
+// Выводит текст каждой строки, которая появляется во входных
+// данных более одного раза. Программа читает список именованных файлов.
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
---
+++ Chapter01/exercise1.4/main.go
@@ -0,0 +1,48 @@
+// Измените программу dup2 так, чтобы она выводила имена всех
+// файлов, в которых найдены повторяющиеся строки.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	lineLocation := make(map[string][]string)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, lineLocation)
+	} else {
+		for _, filename := range files {
+			f, err := os.Open(filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "excercise1.4: %v\n", err)
+				continue
+			}
+			countLines(f, counts, lineLocation)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%v\t%s\n", n, strings.Join(lineLocation[line], " "), line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int, lineLocation map[string][]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		counts[line]++
+		if !slices.Contains(lineLocation[line], f.Name()) {
+			lineLocation[line] = append(lineLocation[line], f.Name())
+		}
+	}	
+	// Примечание: игнорируем потенциальные ошибки из input.Err()
+}
